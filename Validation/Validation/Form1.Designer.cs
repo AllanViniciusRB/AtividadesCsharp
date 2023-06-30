2d61ecb6ca0fdefdbb8cae6517760f6872f48fdf
Allan_Vinicius <allan.vralmeida@senacsp.edu.br>
2023-06-30 15:18:34 -0300

    0

--- Validation/Validation/Form1.Designer.cs
+++ Validation/Validation/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WinFormsApp2
+﻿namespace Validation
 {
     partial class Form1
     {
@@ -28,55 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            comboBox1 = new ComboBox();
-            maskedTextBox1 = new MaskedTextBox();
             checkBox1 = new CheckBox();
-            button1 = new Button();
+            checkBox2 = new CheckBox();
+            Rbn = new RadioButton();
             SuspendLayout();
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(245, 126);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
-            comboBox1.TabIndex = 0;
-            // 
-            // maskedTextBox1
-            // 
-            maskedTextBox1.Location = new Point(324, 294);
-            maskedTextBox1.Name = "maskedTextBox1";
-            maskedTextBox1.Size = new Size(100, 23);
-            maskedTextBox1.TabIndex = 1;
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(607, 125);
+            checkBox1.Location = new Point(282, 185);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(83, 19);
-            checkBox1.TabIndex = 2;
+            checkBox1.TabIndex = 0;
             checkBox1.Text = "checkBox1";
             checkBox1.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // checkBox2
             // 
-            button1.Location = new Point(309, 74);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 3;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(448, 110);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(83, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Rbn
+            // 
+            Rbn.AutoSize = true;
+            Rbn.Location = new Point(175, 198);
+            Rbn.Name = "Rbn";
+            Rbn.Size = new Size(94, 19);
+            Rbn.TabIndex = 2;
+            Rbn.TabStop = true;
+            Rbn.Text = "radioButton1";
+            Rbn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(Rbn);
+            Controls.Add(checkBox2);
             Controls.Add(checkBox1);
-            Controls.Add(maskedTextBox1);
-            Controls.Add(comboBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -85,9 +80,8 @@
 
         #endregion
 
-        private ComboBox comboBox1;
-        private MaskedTextBox maskedTextBox1;
         private CheckBox checkBox1;
-        private Button button1;
+        private CheckBox checkBox2;
+        private RadioButton Rbn;
     }
 }
